24042b72c924059bd8b9027a918f52f26846e230
Pedro Erico <ericopep@hotmail.com>
2023-02-08 23:07:12 -0300

    Criar entity de usuário e uma nova migrate
    - Criado a entidade User para realiza autenticação
    - criado o repositorio
    - Criado a migrate
    - Adicionado no security

--- migrations/Version20230209090651.php
+++ migrations/Version20230209090651.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230209013442 extends AbstractMigration
+final class Version20230209090651 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,14 @@ final class Version20230209013442 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE driver_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE vehicle_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE driver (id INT NOT NULL, vehicle_id INT NOT NULL, name VARCHAR(255) NOT NULL, document VARCHAR(14) NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_11667CD9D8698A76 ON driver (document)');
         $this->addSql('CREATE INDEX IDX_11667CD9545317D1 ON driver (vehicle_id)');
         $this->addSql('COMMENT ON COLUMN driver.updated_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN driver.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
         $this->addSql('CREATE TABLE vehicle (id INT NOT NULL, name VARCHAR(255) NOT NULL, plate VARCHAR(7) NOT NULL, color VARCHAR(255) DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1B80E486719ED75B ON vehicle (plate)');
         $this->addSql('COMMENT ON COLUMN vehicle.updated_at IS \'(DC2Type:datetime_immutable)\'');
@@ -38,10 +39,10 @@ final class Version20230209013442 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE driver_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE vehicle_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
         $this->addSql('ALTER TABLE driver DROP CONSTRAINT FK_11667CD9545317D1');
         $this->addSql('DROP TABLE driver');
+        $this->addSql('DROP TABLE "user"');
         $this->addSql('DROP TABLE vehicle');
     }
 }
