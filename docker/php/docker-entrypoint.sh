c24961ff4a4a84c001a38ee055d903287ad14fe9
Pedro Erico <ericopep@hotmail.com>
2023-02-09 22:19:24 -0300

    Corrigir comandos do docker
    - Foi corrigido o script de comandos pois estava dando erro
    com banco sqlite

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -4,17 +4,19 @@ set -e
 #
 mkdir -p /var/www
 
+rm /var/www/var/*.db || true
+
 composer i -o
 
 php /var/www/bin/console cache:clear
 
 php /var/www/bin/console doctrine:database:create --if-not-exists --no-interaction
 
-php /var/www/bin/console --env=test doctrine:database:create --if-not-exists --no-interaction
+php /var/www/bin/console --env=test doctrine:database:create
 
 php /var/www/bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
-php /var/www/bin/console --env=test doctrine:schema:create --no-interaction --allow-no-migration
+php /var/www/bin/console --env=test doctrine:schema:create --no-interaction
 
 php /var/www/bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
 
